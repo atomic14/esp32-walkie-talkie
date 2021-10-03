ab6d1cc0640ba99c1f18569bcaa33d19c8354497
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2021-10-03 15:12:29 +0200

    EspNowTransport.h add wifi_channel in constructor

--- lib/transport/src/EspNowTransport.h
+++ lib/transport/src/EspNowTransport.h
@@ -8,7 +8,7 @@ class EspNowTransport: public Transport {
 protected:
   void send();
 public:
-  EspNowTransport(OutputBuffer *output_buffer);
+  EspNowTransport(OutputBuffer *output_buffer, uint8_t wifi_channel);
   virtual bool begin() override;
   friend void receiveCallback(const uint8_t *macAddr, const uint8_t *data, int dataLen);
 };
